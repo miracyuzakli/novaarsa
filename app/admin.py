66c5e37d8f5ec8c5cf9b7cbf6b45b16eadff0db1
miracyuzakli <miracyuzakli@gmail.com>
2023-12-04 09:18:38 +0300

    parcel in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Parcel  # .models, modelin bulunduğu dosyanın adıdır
+
+
+class ParcelAdmin(admin.ModelAdmin):
+    list_display = ('il', 'ilce', 'mevki', 'ada', 'parsel', 'm2_net', 'fiyat', 'durum', 'user_id')
+    list_filter = ('il', 'ilce', 'durum')
+    search_fields = ('ada', 'parsel', 'user_id')
+
+admin.site.register(Parcel, ParcelAdmin)
+
+# admin.site.register(Parcel)
